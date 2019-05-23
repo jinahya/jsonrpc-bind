434b1d09e29e40f1c75d134e2a3db7dea600ffa5
Jin Kwon <onacit@wemakeprice.com>
2019-05-23 18:53:06 +0900

    updated

--- src/main/java/com/github/jinahya/jsonrpc2/types/JsonRpcObject.java
+++ src/main/java/com/github/jinahya/jsonrpc2/types/JsonRpcObject.java
@@ -1,23 +1,52 @@
 package com.github.jinahya.jsonrpc2.types;
 
+/**
+ * An abstract class for request object and response object.
+ * @param <T> self type parameter
+ */
 public abstract class JsonRpcObject<T extends JsonRpcObject<T>> {
 
+    /**
+     * A constant for {@code jsonrpc} attribute. The value is {@value #JSONRPC}.
+     */
     public static final String JSONRPC = "2.0";
 
     // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Returns the current value of {@code jsonrpc} attribute.
+     *
+     * @return the current value of {@code jsonrpc} attribute.
+     */
     public String getJsonrpc() {
         return jsonrpc;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Returns the current value of {@code id} attribute.
+     *
+     * @return the current value of {@code id} attribute.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Replaces current value of {@code id} attribute with given.
+     *
+     * @param id new value for {@code id} attribute.
+     */
     public void setId(final String id) {
         this.id = id;
     }
 
+    /**
+     * Replaces current value of {@code id} attribute with given and returns this instance.
+     *
+     * @param id new value for {@code id} attribute.
+     */
     @SuppressWarnings({"unchecked"})
     public T id(final String id) {
         setId(id);
--- src/main/java/com/github/jinahya/jsonrpc2/types/RequestObject.java
+++ src/main/java/com/github/jinahya/jsonrpc2/types/RequestObject.java
@@ -3,12 +3,39 @@ package com.github.jinahya.jsonrpc2.types;
 import javax.validation.constraints.AssertFalse;
 import javax.validation.constraints.NotEmpty;
 
+/**
+ * A class for request objects.
+ *
+ * @param <T> self type parameter
+ * @param <U> {@code params} type parameter
+ * @see <a href="https://www.jsonrpc.org/specification#request_object">4. Request Object (JSON-RPC 2.0
+ * Specification)</a>
+ */
 public class RequestObject<T extends RequestObject<T, U>, U> extends JsonRpcObject<T> {
 
     // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Checks if {@code method} attribute is starts with reserved for rpc-internal.
+     *
+     * @return {@code true} if reserved, {@code false} otherwise
+     */
     @AssertFalse
     private boolean isMethodReservedForRpcInternal() {
         return method.startsWith("rpc.");
+    }
+
+    // -------------------------------------------------------------------------------------------------------------- id
+
+    /**
+     * Checks if this request object is a notification.
+     *
+     * @return {@code true} if this request object is a notification, {@code false} otherwise
+     * @see <a href="https://www.jsonrpc.org/specification#notification">4.1 Notification (JSON-RPC 2.0
+     * Specification)</a>
+     */
+    public boolean isNotification() {
+        return getId() == null;
     }
 
     // ---------------------------------------------------------------------------------------------------------- method
@@ -21,17 +48,36 @@ public class RequestObject<T extends RequestObject<T, U>, U> extends JsonRpcObje
     }
 
     // ---------------------------------------------------------------------------------------------------------- params
+
+    /**
+     * Returns current value of {@code params} attribute.
+     *
+     * @return current value of {@code params} attribute
+     */
     public U getParams() {
         return params;
     }
 
+    /**
+     * Replaces value of {@code params} attribute with given.
+     *
+     * @param params new value for {@code params} attribute
+     * @return this instance
+     */
     public void setParams(final U params) {
         this.params = params;
     }
 
-    // -------------------------------------------------------------------------------------------------------------- id
-    public boolean isForNotification() {
-        return getId() == null;
+    /**
+     * Replaces the value of {@code params} attribute with given and returns this instance.
+     *
+     * @param params new value for {@code params} attribute
+     * @return this instance
+     */
+    @SuppressWarnings({"unchecked"})
+    public T params(final U params) {
+        setParams(params);
+        return (T) this;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
