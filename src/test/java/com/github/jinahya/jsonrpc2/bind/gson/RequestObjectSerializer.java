7090e0b4a30c5803e71de794d73c291b5a1beac2
Jin Kwon <onacit@gmail.com>
2019-05-26 12:40:05 +0900

    Remove gson related classes in src/test/java

--- src/test/java/com/github/jinahya/jsonrpc2/bind/gson/RequestObjectSerializer.java
+++
@@ -1,7 +0,0 @@
-package com.github.jinahya.jsonrpc2.bind.gson;
-
-import com.github.jinahya.jsonrpc2.bind.RequestObject;
-
-public class RequestObjectSerializer extends JsonrpcObjectSerializer<RequestObject<?>> {
-
-}
