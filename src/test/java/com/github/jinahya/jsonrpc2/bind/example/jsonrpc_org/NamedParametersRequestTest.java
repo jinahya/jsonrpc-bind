146c73cc6fefe175ed52d855670d11754e55b1c3
Jin Kwon <onacit@wemakeprice.com>
2019-05-29 14:29:39 +0900

    updated

--- src/test/java/com/github/jinahya/jsonrpc2/bind/example/jsonrpc_org/NamedParametersRequestTest.java
+++ src/test/java/com/github/jinahya/jsonrpc2/bind/example/jsonrpc_org/NamedParametersRequestTest.java
@@ -1,8 +1,7 @@
 package com.github.jinahya.jsonrpc2.bind.example.jsonrpc_org;
 
 import com.github.jinahya.jsonrpc2.bind.GsonUtils;
-import com.github.jinahya.jsonrpc2.bind.JacksonUtils;
-import com.github.jinahya.jsonrpc2.bind.JsonbUtils;
+import com.github.jinahya.jsonrpc2.bind.JsonTests;
 import com.github.jinahya.jsonrpc2.bind.MoshiUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,43 +11,20 @@ import java.io.IOException;
 @Slf4j
 class NamedParametersRequestTest {
 
-    // -----------------------------------------------------------------------------------------------------------------
     @Test
-    void jackson01() throws IOException {
-        final NamedParametersRequest value = JacksonUtils.readResource(
-                "/examples/jsonrpc_org/named_parameters_01_request.json", NamedParametersRequest.class);
-        log.debug("value: {}", value);
-        log.debug("value.id.class: {}", value.getId().getClass());
-        log.debug("json: {}", JacksonUtils.OBJECT_MAPPER.writeValueAsString(value));
+    void request01() throws IOException {
+        JsonTests.doWithResource(
+                "/examples/jsonrpc_org/named_parameters_01_request.json",
+                NamedParametersRequest.class
+        );
     }
 
     @Test
-    void jackson02() throws IOException {
-        final NamedParametersRequest value = JacksonUtils.readResource(
-                "/examples/jsonrpc_org/named_parameters_02_request.json", NamedParametersRequest.class);
-        log.debug("value: {}", value);
-        log.debug("value.params: {}", value.getParams());
-        log.debug("value.id.class: {}", value.getId().getClass());
-        log.debug("json: {}", JacksonUtils.OBJECT_MAPPER.writeValueAsString(value));
-    }
-
-    // -----------------------------------------------------------------------------------------------------------------
-    @Test
-    void jsonb01() throws IOException {
-        final NamedParametersRequest value = JsonbUtils.fromResource(
-                "/examples/jsonrpc_org/named_parameters_01_request.json", NamedParametersRequest.class);
-        log.debug("value: {}", value);
-        log.debug("value.id.class: {}", value.getId().getClass());
-        log.debug("json: {}", JsonbUtils.JSONB.toJson(value));
-    }
-
-    @Test
-    void jsonb02() throws IOException {
-        final NamedParametersRequest value = JsonbUtils.fromResource(
-                "/examples/jsonrpc_org/named_parameters_02_request.json", NamedParametersRequest.class);
-        log.debug("value: {}", value);
-        log.debug("value.id.class: {}", value.getId().getClass());
-        log.debug("json: {}", JsonbUtils.JSONB.toJson(value));
+    void request02() throws IOException {
+        JsonTests.doWithResource(
+                "/examples/jsonrpc_org/named_parameters_02_request.json",
+                NamedParametersRequest.class
+        );
     }
 
     // -----------------------------------------------------------------------------------------------------------------
