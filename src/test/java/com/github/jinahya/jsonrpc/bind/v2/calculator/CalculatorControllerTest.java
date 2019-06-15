26897a1208fdc97a3bf39baf15c3595bc0f95cfb
Jin Kwon <onacit@gmail.com>
2019-06-15 22:46:28 +0900

    Format the whose src/

--- src/test/java/com/github/jinahya/jsonrpc/bind/v2/calculator/CalculatorControllerTest.java
+++ src/test/java/com/github/jinahya/jsonrpc/bind/v2/calculator/CalculatorControllerTest.java
@@ -9,9 +9,9 @@ package com.github.jinahya.jsonrpc.bind.v2.calculator;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -73,7 +73,8 @@ public class CalculatorControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
-        final CalculatorResponse calculatorResponse = OBJECT_MAPPER.readValue(mvcResult.getResponse().getContentAsString(), CalculatorResponse.class);
+        final CalculatorResponse calculatorResponse = OBJECT_MAPPER.readValue(
+                mvcResult.getResponse().getContentAsString(), CalculatorResponse.class);
         assertNull(calculatorResponse.getError());
         final BigDecimal result = calculatorResponse.getResult();
         assertEquals(BigDecimal.ONE, result);
@@ -96,7 +97,8 @@ public class CalculatorControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
-        final CalculatorResponse calculatorResponse = OBJECT_MAPPER.readValue(mvcResult.getResponse().getContentAsString(), CalculatorResponse.class);
+        final CalculatorResponse calculatorResponse = OBJECT_MAPPER.readValue(
+                mvcResult.getResponse().getContentAsString(), CalculatorResponse.class);
         assertNull(calculatorResponse.getError());
         final BigDecimal result = calculatorResponse.getResult();
         assertEquals(BigDecimal.ONE, result);
@@ -119,7 +121,8 @@ public class CalculatorControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
-        final CalculatorResponse calculatorResponse = OBJECT_MAPPER.readValue(mvcResult.getResponse().getContentAsString(), CalculatorResponse.class);
+        final CalculatorResponse calculatorResponse = OBJECT_MAPPER.readValue(
+                mvcResult.getResponse().getContentAsString(), CalculatorResponse.class);
         assertNull(calculatorResponse.getError());
         final BigDecimal result = calculatorResponse.getResult();
         assertEquals(BigDecimal.ZERO, result);
