00c7ba51d1d9b4e5945c79331a263caa5dc3401c
Jin Kwon <onacit@gmail.com>
2019-06-15 22:44:20 +0900

    Remove unused files

--- src/test/java/com/github/jinahya/jsonrpc/bind/v2/johnzon123/SomeOptions.java
+++
@@ -1,5 +0,0 @@
-package com.github.jinahya.jsonrpc.bind.v2.johnzon123;
-
-public class SomeOptions extends Options {
-
-}
--- src/test/java/com/github/jinahya/jsonrpc/bind/v2/johnzon123/SomeOptionsAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.github.jinahya.jsonrpc.bind.v2.johnzon123;
-
-public class SomeOptionsAdapter extends OptionsAdapter<SomeOptions> {
-
-    public SomeOptionsAdapter() {
-        super(SomeOptions.class);
-    }
-}
