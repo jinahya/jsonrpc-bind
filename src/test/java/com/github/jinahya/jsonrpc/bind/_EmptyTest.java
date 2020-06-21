a1fe5471865402aa242d8983b3a8e619697dc992
Jin Kwon <onacit@users.noreply.github.com>
2020-06-21 11:38:12 +0900

    Add an emtpty (public) test class

---
+++ src/test/java/com/github/jinahya/jsonrpc/bind/_EmptyTest.java
@@ -0,0 +1,11 @@
+package com.github.jinahya.jsonrpc.bind;
+
+/**
+ * Just an empty class.
+ */
+public class _EmptyTest {
+
+    private _EmptyTest() {
+        throw new AssertionError("instantiation is not allowed");
+    }
+}
